54d497d4bb9ed3de9b3ab6a84de93b5e47336137
ajtrao <114036414+ajtrao@users.noreply.github.com>
2022-09-26 20:48:31 -0400

    adding java script file

---
+++ javascript/javascriptclass1.js
@@ -0,0 +1,61 @@
+
+// initialization
+var name="car";
+console.log(name);
+
+//object declaration
+var car=
+{
+    name:"bmw",
+    model:"sports",
+    year:2022
+}
+console.log(car);
+console.log(car.model);
+
+//array of objects
+var car_1=[
+    {
+    name:"bmw",
+    model:"sports",
+    year:2022
+    },
+    {
+    name:"bmw",
+    model:"luxery",
+    year:2021
+    }
+    ]
+console.log(car_1.model);
+
+
+var bike =
+{
+    brand : "Royal Enfield",
+    model : "350x",
+    year  : "2019"
+}
+console.log(bike.year);
+
+var bike_1= [
+    {
+    brand : "Royal Enfield",
+    model : "350x",
+    year  : "2019",
+    color : "white"
+    
+    },
+    {
+    brand : "Royal Enfield",
+    model : "550x",
+    year  : "2022",
+    color : "red"
+    },
+    {
+    brand : "Royal Enfield",
+    model : "350x",
+    year  : "2022",
+    color : "yellow"
+    }
+    ]
+console.log(bike_1);
