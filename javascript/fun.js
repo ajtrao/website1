405fabc5573f5f388cf091685fcadb4bb906d1ba
ajtrao <114036414+ajtrao@users.noreply.github.com>
2022-09-30 21:36:35 -0400

    javascript code added to fun.js

--- javascript/fun.js
+++ javascript/fun.js
@@ -44,3 +44,32 @@ function restuarents(){
      }
 }
 restuarents();
+
+// example 3
+
+function company(){
+    const companies= [
+        {name: "Company One", category: "Finance", start: 1981, end: 2004},
+        {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+        {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+        {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+        {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+        {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+        {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+        {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+        {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+    ];
+    
+    for (var i=0; i<companies.length; i++){
+        if (companies[i].category== "Retail"){
+            console.log(companies[i].category);
+        }
+    }
+    
+    for (var i=0; i<companies.length; i++){
+        if ( companies[i].start < 2000 && companies[i].end > 2005 ){
+            console.log(companies[i].name);
+        }
+    }
+} 
+company();
